f5bd0e201a8a5b42f365d1eec99fcea19e01d608
Anton Melnikov <anton@addstructure.com>
2017-12-15 16:18:52 -0600

    Override phony __len__ value from superclass

--- onmt/IO.py
+++ onmt/IO.py
@@ -267,6 +267,9 @@ class OrderedIterator(torchtext.data.Iterator):
                                           self.batch_size_fn):
                 self.batches.append(sorted(b, key=self.sort_key))
 
+    def __len__(self):
+        return None
+
 
 class ONMTDataset(torchtext.data.Dataset):
     """
